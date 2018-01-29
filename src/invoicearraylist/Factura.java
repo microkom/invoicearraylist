21717fca0858ca05afa2d6a062950d2f10a6d4e5
German <34686118+microkom@users.noreply.github.com>
2018-01-29 10:58:19 +0000

    Update Factura.java

--- src/invoicearraylist/Factura.java
+++ src/invoicearraylist/Factura.java
@@ -1,25 +1,53 @@
 package invoicearraylist;
-import static java.lang.Math.floor;import static java.lang.Math.round;import java.util.ArrayList;import java.util.Iterator;
+import static java.lang.Math.floor;
+import static java.lang.Math.round;
+import java.util.ArrayList;
+import java.util.Iterator;
 public class Factura {
-    private int numero;    private Cliente cliente;    private Fecha fecha;    //private LineaFactura[] lineas = new LineaFactura[10];    ArrayList<LineaFactura> lineas = new ArrayList<LineaFactura>();
-    Textos texto = new Textos();
-    public Factura(int numero, Cliente cliente, Fecha fecha) {        this.numero = numero;        this.cliente = cliente;        this.fecha = fecha;    }
-    public int getNumero() {        return this.numero;    }
+    private int numero;
+  private Cliente cliente;
+  private Fecha fecha;
+  
+  Textos texto = new Textos();
+  
+  public Factura(int numero, Cliente cliente, Fecha fecha) {
+      this.numero = numero;
+      this.cliente = cliente;
+      this.fecha = fecha;
+    }
+  public int getNumero() {        return this.numero;    }
     public Cliente getCliente() {        return this.cliente;    }
     public Fecha getFecha() {        return this.fecha;    }
     public ArrayList<LineaFactura> getLineas() {        return this.lineas;    }
     public void setCliente(Cliente cliente) {        this.cliente = cliente;    }
     public void setFecha(Fecha fecha) {        this.fecha = fecha;    }
-    public void setLineaFactura(ArrayList<LineaFactura> lineas) {        this.lineas = lineas;    }
-    /*    nuevaLinea(LineaFactura lf): Añadir una línea a la factura. En caso    que lo haya podido añadir debe devolver el número de la linea en la    que la ha insertado. En caso contrario (vector lleno) debe devolver -    1.      */     
-    public int nuevaLinea(ArrayList<LineaFactura> lineaf) {                Iterator<LineaFactura> iterator = lineaf.iterator();        LineaFactura lf = null;                int vector = -1;        int i, j;        boolean insertado = false;                    j = buscarProducto(lf.getProducto().getNombre());
-            //comprobación de que el producto ya aparece en la factura o no             if (j != -1) {                //suma de cantidades                 this.lineas.get(j).setCantidad(lineas.get(j).getCantidad() + lf.getCantidad());                this.lineas.get(j).setDescuento(lf.getDescuento());            } else {                //Inserción de objeto en indice de array                i=0;                while (iterator.hasNext() && insertado == false) {                    if (this.lineas.get(i)==null) {                        lineas.add(i, lf);                        insertado = true;                        vector = i;                                           }                     i++;                }            }            return vector;        }        //borrarLineaNumero(int numLinea) : Elimina la linea con el número indicado    public void borrarLineaNumero(int numLinea) {        //borrado del vector recibiendo la posición exacta         this.lineas.remove(numLinea);
+    public void setLineaFactura(ArrayList<LineaFactura> lineas) {
+      this.lineas = lineas;    
+    }
+    /*    nuevaLinea(LineaFactura lf): Añadir una línea a la factura. 
+    En caso    que lo haya podido añadir debe devolver el número de la linea en la
+    que la ha insertado. En caso contrario (vector lleno) debe devolver -    1.      */     
+    public int nuevaLinea(ArrayList<LineaFactura> lineaf) {
+      Iterator<LineaFactura> iterator = lineaf.iterator();
+      LineaFactura lf = null;                
+      int vector = -1;
+      int i, j;        boolean insertado = false;                    
+      j = buscarProducto(lf.getProducto().getNombre());
+            //comprobación de que el producto ya aparece en la factura o no             
+      if (j != -1) {                
+        //suma de cantidades                 
+        this.lineas.get(j).setCantidad(lineas.get(j).getCantidad() + lf.getCantidad());                
+        this.lineas.get(j).setDescuento(lf.getDescuento());            } else {                
+        //Inserción de objeto en indice de array                
+        i=0;                
+        while (iterator.hasNext() && insertado == false) {                    if (this.lineas.get(i)==null) {                        lineas.add(i, lf);                        insertado = true;                        vector = i;                                           }                     i++;                }            }            return vector;        }        //borrarLineaNumero(int numLinea) : Elimina la linea con el número indicado    public void borrarLineaNumero(int numLinea) {        //borrado del vector recibiendo la posición exacta         this.lineas.remove(numLinea);
         //agrupar el contenido del vector al inicio        ordenVector();    }
     //Agrupación del vector al inicio cuando tiene espacios blancos    public void ordenVector() {        Iterator<LineaFactura> iterator = lineas.iterator();        int i=0;        while (iterator.hasNext()) {            //buscar lineas que no están vacías            if (lineas.get(i) != null) {                boolean found = false;
                 //búsqueda del primer null en el array y reemplazo con el último not null                Iterator<LineaFactura> iterator2 = lineas.iterator();                int j=0;                while (iterator2.hasNext() && found == false ) {                    if (lineas.get(j) == null) {                        lineas.add(j, lineas.get(i));                        lineas.remove(i);                        found = true;                    }                    j++;                }            }            i++;        }    }
     /*    borrarLineaNombre(String nombre): Elimina la linea con el producto indicado     (buscar por nombre de producto).                                        */    public void borrarLineaNombre(String nombre) {        boolean encontrado = false;                Iterator<LineaFactura> iterator = lineas.iterator();                int i =0;        while (iterator.hasNext() && encontrado == false){            //saltarse las campos del vector vacio            if (lineas.get(i) != null) {                //busqueda del nombre recibido por parametro con el vector actual en i                if (this.lineas.get(i).getProducto().getNombre().equals(nombre)) {                    //borrado del contenido de una casilla del vector                    this.lineas.remove(i);                    encontrado = true;                                    }            }            i++;        }        //ordenVector();    }
     //importeTotal(): Calcular el importe total.//importeTotal(): Calcular el importe total.    public double importeTotal() {        double total = 0;        int i=0;                Iterator<LineaFactura> iterator = lineas.iterator();                while (iterator.hasNext()) {            //saltarse las campos del vector vacio
-            if (this.lineas.get(i) != null) {                //sumar los precios de todos los productos                total += (this.lineas.get(i).getProducto().getPrecio() * this.lineas.get(i).getCantidad());            }            i++;        }        return total;    }
+            if (this.lineas.get(i) != null) {
+              //sumar los precios de todos los productos                total += (this.lineas.get(i).getProducto().getPrecio() * this.lineas.get(i).getCantidad());            }            i++;        }        return total;    }
     public double descuentoTotal() {        double desc = 0;        int i=0;        Iterator<LineaFactura> iterator = lineas.iterator();                while (iterator.hasNext()) {            //saltarse las campos del vector vacio            if (this.lineas.get(i) != null) {                //sumar los descuentos de todos los productos                desc += this.lineas.get(i).getProducto().getPrecio() * this.lineas.get(i).getCantidad() * this.lineas.get(i).getDescuento();
             }            i++;        }        return desc;    }
     //ImporteTotalImpuestos(): Devuelve el importe total con el iva(21%).    public double importeTotalImpuestos() {        //calculo del importe total más el IVA        return (importeTotal() - descuentoTotal()) * 1.21;    }
